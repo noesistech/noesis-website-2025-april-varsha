be2dd1778314c239c1506a749573e38f2cb6e8fb
grsawardekarnoesis <ganesh@noesis.tech>
2025-05-22 15:20:55 +0530

    brand logo updated and ui fixes in chatbot

--- src/data/content/clients.ts
+++ src/data/content/clients.ts
@@ -16,7 +16,7 @@ export const clientLogosData: ClientLogo[] = [
   {
     id: 'client-logo-1',
     name: 'NowYouKnow',
-    image_url: '/lovable-uploads/2af3e4c9-e92b-4985-b118-932ae007dc08.png',
+    image_url: '/lovable-uploads/nowyouknow.png',
     sort_order: 1,
     created_at: '',
     updated_at: '',
@@ -24,7 +24,7 @@ export const clientLogosData: ClientLogo[] = [
   {
     id: 'client-logo-2',
     name: 'Hindware',
-    image_url: '/lovable-uploads/e171ca53-42a5-4dca-bed9-bcee5a3d6faa.png',
+    image_url: '/lovable-uploads/hindware.png',
     sort_order: 2,
     created_at: '',
     updated_at: '',
@@ -32,7 +32,7 @@ export const clientLogosData: ClientLogo[] = [
   {
     id: 'client-logo-3',
     name: 'Cathedral and John Connon School',
-    image_url: '/lovable-uploads/cafd8b63-706f-4f10-82e5-4cf9c82cdad5.png',
+    image_url: '/lovable-uploads/cathedral.png',
     sort_order: 3,
     created_at: '',
     updated_at: '',
@@ -40,7 +40,7 @@ export const clientLogosData: ClientLogo[] = [
   {
     id: 'client-logo-4',
     name: 'Origem',
-    image_url: '/lovable-uploads/b54fac88-3555-4650-8a2c-8e296fe3711a.png',
+    image_url: '/lovable-uploads/origem.png',
     sort_order: 4,
     created_at: '',
     updated_at: '',
@@ -48,7 +48,7 @@ export const clientLogosData: ClientLogo[] = [
   {
     id: 'client-logo-5',
     name: 'RevitalH',
-    image_url: '/lovable-uploads/537d148b-a0fd-4d26-90b5-cf8fcaa8fc34.png',
+    image_url: '/lovable-uploads/revital.png',
     sort_order: 5,
     created_at: '',
     updated_at: '',
@@ -56,7 +56,7 @@ export const clientLogosData: ClientLogo[] = [
   {
     id: 'client-logo-6',
     name: 'Education Above All',
-    image_url: '/lovable-uploads/a0ef532f-efb3-4602-b66f-e2a62a584113.png',
+    image_url: '/lovable-uploads/EAA.png',
     sort_order: 6,
     created_at: '',
     updated_at: '',
@@ -64,7 +64,7 @@ export const clientLogosData: ClientLogo[] = [
   {
     id: 'client-logo-7',
     name: 'Poshan Atlas',
-    image_url: '/lovable-uploads/65864139-00c7-45d3-ad39-1345efa8869a.png',
+    image_url: '/lovable-uploads/poshanatlas.png',
     sort_order: 7,
     created_at: '',
     updated_at: '',
@@ -72,11 +72,19 @@ export const clientLogosData: ClientLogo[] = [
   {
     id: 'client-logo-8',
     name: 'Indibid',
-    image_url: '/lovable-uploads/604a9999-53b8-43bb-8555-568b20b37d1a.png',
+    image_url: '/lovable-uploads/indibid.png',
     sort_order: 8,
     created_at: '',
     updated_at: '',
   },
+  {
+    id: 'client-logo-9',
+    name: 'Indibid',
+    image_url: '/lovable-uploads/brainstormer.png',
+    sort_order: 8,
+    created_at: '',
+    updated_at: '',
+  }
 ];
 
 export const partnerLogosData = [];
